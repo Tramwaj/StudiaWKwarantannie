8c9a6f133039ffc9d9c53b53856df682b2aa52b9
Michał Salamon <ms055452@student.ath.edu.pl>
2020-05-03 11:06:46 +0200

    prototype the main window

---
+++ Tasker/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Tasker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnEventDetails = new System.Windows.Forms.Button();
+            this.btnDeleteEvent = new System.Windows.Forms.Button();
+            this.btnAddEvent = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.lstDate = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstUpcoming = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(4, 12);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(352, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lstDate);
+            this.splitContainer1.Panel1.Controls.Add(this.btnEventDetails);
+            this.splitContainer1.Panel1.Controls.Add(this.btnDeleteEvent);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lstUpcoming);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(615, 562);
+            this.splitContainer1.SplitterDistance = 309;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // btnEventDetails
+            // 
+            this.btnEventDetails.Location = new System.Drawing.Point(49, 467);
+            this.btnEventDetails.Name = "btnEventDetails";
+            this.btnEventDetails.Size = new System.Drawing.Size(237, 41);
+            this.btnEventDetails.TabIndex = 2;
+            this.btnEventDetails.Text = "Szczegóły";
+            this.btnEventDetails.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteEvent
+            // 
+            this.btnDeleteEvent.ForeColor = System.Drawing.Color.Red;
+            this.btnDeleteEvent.Location = new System.Drawing.Point(211, 524);
+            this.btnDeleteEvent.Name = "btnDeleteEvent";
+            this.btnDeleteEvent.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteEvent.TabIndex = 1;
+            this.btnDeleteEvent.Text = "Usuń";
+            this.btnDeleteEvent.UseVisualStyleBackColor = true;
+            this.btnDeleteEvent.Click += new System.EventHandler(this.btnDeleteEvent_Click);
+            // 
+            // btnAddEvent
+            // 
+            this.btnAddEvent.Location = new System.Drawing.Point(12, 251);
+            this.btnAddEvent.Name = "btnAddEvent";
+            this.btnAddEvent.Size = new System.Drawing.Size(166, 67);
+            this.btnAddEvent.TabIndex = 0;
+            this.btnAddEvent.Text = "Dodaj";
+            this.btnAddEvent.UseVisualStyleBackColor = true;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // lstDate
+            // 
+            this.lstDate.FormattingEnabled = true;
+            this.lstDate.ItemHeight = 16;
+            this.lstDate.Location = new System.Drawing.Point(4, 4);
+            this.lstDate.Name = "lstDate";
+            this.lstDate.Size = new System.Drawing.Size(302, 436);
+            this.lstDate.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 467);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Szczegóły";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lstUpcoming
+            // 
+            this.lstUpcoming.FormattingEnabled = true;
+            this.lstUpcoming.ItemHeight = 16;
+            this.lstUpcoming.Location = new System.Drawing.Point(4, 4);
+            this.lstUpcoming.Name = "lstUpcoming";
+            this.lstUpcoming.Size = new System.Drawing.Size(295, 436);
+            this.lstUpcoming.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(966, 586);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.btnAddEvent);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnDeleteEvent;
+        private System.Windows.Forms.Button btnAddEvent;
+        private System.Windows.Forms.Button btnEventDetails;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.ListBox lstDate;
+        private System.Windows.Forms.ListBox lstUpcoming;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Tasker/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tasker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            //monthCalendar1.
+        }
+
+        private void btnDeleteEvent_Click(object sender, EventArgs e)
+        {
+            DialogResult deleteConfirmed = MessageBox.Show(
+                "Czy naprawdę usunąć zapis?", "Usunięcie", MessageBoxButtons.YesNo);            
+        }
+    }
+}
