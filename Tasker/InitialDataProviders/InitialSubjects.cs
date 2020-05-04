5e0ce34327d2fa867506706c059cd835d6500168
Michał Salamon <ms055452@student.ath.edu.pl>
2020-05-04 20:29:40 +0200

    fill example Subjects

--- Tasker/InitialDataProviders/InitialSubjects.cs
+++ Tasker/InitialDataProviders/InitialSubjects.cs
@@ -13,10 +13,14 @@ namespace Tasker.InitialDataProviders
         {
             return new List<Subject>
             {
-                new Subject("Programowanie 4 - lab",teachers.Single(x=>x.LastName=="Warwas"),9,5),
-                new Subject("");
+                new Subject("Programowanie 4 - lab", "P4",
+                teachers.Single(t=>t.LastName=="Warwas"), 3, 9),
+                new Subject("Metody Sztucznej inteligencji", "MSI",
+                teachers.Single(t=>t.LastName=="Kordos"), 6, 5),
+                new Subject("Grafika Komputerowa", "GK",
+                teachers.Single(t=>t.LastName=="Ilewicz"), 4, 3)
                 /* J.ang. 1 ; Md 2 ; Boio 3 ; P4 3 ; SK 6 ; GK 4 ; MSI 6 ; BD 5 */
-            }
+            };
         }
     }
 }
